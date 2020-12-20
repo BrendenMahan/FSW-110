01ce9040939b44f61c3166e15dce6cd5c6dde18e
BrendenMahan <71709556+BrendenMahan@users.noreply.github.com>
2020-12-20 15:49:26 -0600

    Add files via upload

---
+++ FSW-110/Week5/Modifying an Element’s Value/index.js
@@ -0,0 +1,11 @@
+function getdetails(){
+
+    var l = document.forms["details"]["lastname"].value;
+
+    var f = document.forms["details"]["firstname"].value;
+
+    var e = document.forms["details"]["email"].value;
+
+    alert("Your name is: "+l+" "+f+". Your e-mail is: "+e);
+    }
+    
